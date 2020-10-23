782975396ac101f72d02be5dc39dc0ad6699e82a
mujumdarsmita <smitamujumdar94@gmail.com>
2020-10-22 22:54:53 -0700

    Added camera service. Implemented logOut and SignUp feature.

--- app/src/main/java/com/example/picstagram/LoginActivity.java
+++ app/src/main/java/com/example/picstagram/LoginActivity.java
@@ -1,7 +1,6 @@
 package com.example.picstagram;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,20 +8,26 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class LoginActivity extends AppCompatActivity {
 
   EditText etUsername;
   EditText etPassword;
   Button btnLogin;
+  Button btnSignUp;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_login);
+
+
 
     if(ParseUser.getCurrentUser()!=null){
       goMainActivity();
@@ -31,6 +36,7 @@ public class LoginActivity extends AppCompatActivity {
     etUsername = findViewById(R.id.etUsername);
     etPassword = findViewById(R.id.etPassword);
     btnLogin = findViewById(R.id.btnLogin);
+    btnSignUp = findViewById(R.id.btnSignUp);
 
     btnLogin.setOnClickListener(new View.OnClickListener() {
       @Override
@@ -41,6 +47,35 @@ public class LoginActivity extends AppCompatActivity {
       }
     });
 
+    // on clicking sign up botton
+    btnSignUp.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        String username = etUsername.getText().toString();
+        String password = etPassword.getText().toString();
+        userSignUp(username, password);
+      }
+    });
+
+  }
+
+  private void userSignUp(String username, String password) {
+    Toast.makeText(getApplicationContext(), "user SignUp", Toast.LENGTH_SHORT).show();
+
+    // creating Parser user
+    ParseUser user = new ParseUser();
+    user.setUsername(username);
+    user.setPassword(password);
+    user.signUpInBackground(new SignUpCallback() {
+      @Override
+      public void done(ParseException e) {
+        if(e!=null){
+          Log.e("LoginActivity", "Issue with Signing up", e);
+        }
+        Toast.makeText(getApplicationContext(), "Sign up success", Toast.LENGTH_SHORT).show();
+        goMainActivity();
+      }
+    });
   }
 
   private void userLogin(String username, String password) {
@@ -49,7 +84,7 @@ public class LoginActivity extends AppCompatActivity {
       @Override
       public void done(ParseUser user, ParseException e) {
         if (e != null) {
-          Log.e("LoginActivity", "Issue with login", e);
+          Log.e("LoginActivity", "Issue with log in", e);
           return;
         }
         Toast.makeText(getApplicationContext(), "login success", Toast.LENGTH_SHORT).show();
---
+++ app/src/main/java/com/example/picstagram/ProfileActivity.java
@@ -0,0 +1,37 @@
+package com.example.picstagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.parse.ParseUser;
+
+public class ProfileActivity extends AppCompatActivity {
+
+  Button btnLogout;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_profile);
+
+    btnLogout = findViewById(R.id.btnLogout);
+    btnLogout.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        ParseUser.logOut();
+        goLoginActivity();
+      }
+    });
+
+
+  }
+
+  private void goLoginActivity() {
+    Intent intent = new Intent(this, LoginActivity.class);
+    startActivity(intent);
+  }
+}
