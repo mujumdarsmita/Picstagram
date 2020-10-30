347000cd872fbf42b6b197a797adaa72716f8051
mujumdarsmita <smitamujumdar94@gmail.com>
2020-10-30 01:13:13 -0700

    Implemented pull to refresh

--- app/src/main/java/com/example/picstagram/fragments/TimelineFragment.java
+++ app/src/main/java/com/example/picstagram/fragments/TimelineFragment.java
@@ -7,6 +7,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -29,6 +30,7 @@ public class TimelineFragment extends Fragment {
   public static final String TAG = "TimelineFragment";
   protected PostAdapter adapter;
   protected List<Post> allPosts;
+  SwipeRefreshLayout swipeContainer;
 
   // TODO: Rename parameter arguments, choose names that match
   // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -49,11 +51,26 @@ public class TimelineFragment extends Fragment {
                            Bundle savedInstanceState) {
     // Inflate the layout for this fragment
     return inflater.inflate(R.layout.fragment_timeline, container, false);
+
   }
 
   @Override
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
+    swipeContainer = view.findViewById(R.id.swipeContainer);
+    swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+      @Override
+      public void onRefresh() {
+        queryPosts();
+        swipeContainer.setRefreshing(false);
+      }
+    });
+
+    swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                                           android.R.color.holo_green_light,
+                                           android.R.color.holo_orange_light,
+                                           android.R.color.holo_red_light);
+
     rvPosts = view.findViewById(R.id.rvPosts);
     allPosts = new ArrayList<>();
     adapter = new PostAdapter(getContext(), allPosts);
